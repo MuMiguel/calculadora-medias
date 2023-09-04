817146ab99773455642da297e40f60dc65e8465c
Murilo Miguel <murilo.miguel0801@gmail.com>
2023-09-04 15:31:32 +0100

    fix: limitação de casas decimais

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ function adicionaLinha() {
     function atualizaMediaFinal() {
         const mediaFinal = calculaMediaFinal ();
 
-        document.getElementById('mediaFinalValor').innerHTML = mediaFinal;
+        document.getElementById('mediaFinalValor').innerHTML = mediaFinal.toFixed(2);
         document.getElementById('mediaFinalResultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
     }
 
